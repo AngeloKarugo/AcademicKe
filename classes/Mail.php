d4097ad6f995b8c21e14a025005b26cd4bebad4f
Angelo Karugo <akarugo2@gmail.com>
2020-06-18 15:30:59 +0300

    modified sign-up system

--- classes/Mail.php
+++ classes/Mail.php
@@ -1,24 +1,75 @@
 <?php
-    require_once ('PHPMailer/PHPMailerAutoload.php');
+require_once('PHPMailer/PHPMailerAutoload.php');
 
-     class Mail{
-         public static function send_mail($subject, $body, $address){
-            $mail = new PHPMailer();
-            $mail-> isSMTP();
-            $mail-> SMTPauth = true;
-            $mail-> SMTPSecure = 'ssl';
-            $mail-> host = 'smtp.gmail.com';
-            $mail-> port = '465';
-            $mail-> isHTML();
-            $mail-> Username = '';
-            $mail-> Password = '';
-            $mail-> SetFrom('akarugo2@gmail.com');
-            $mail-> Subject = $subject;
-            $mail-> Body = $body;
-            $mail-> AddAddress($address);
+class Mail
+{
+    public static function send_mail($to, $msg, $subject, $from = '')
+    {
 
-            $mail-> Send();
-         }
-     }
+        //    required headers
 
-?>
+        $stkheader = array('Content-Type:application/json', 'Cache-Control:no-cache');
+
+        //    url to post the data
+
+        $url = 'https://oyaa.co.ke/api/sendMail.php';
+
+
+
+        # initiating curl
+
+        $curl = curl_init();
+
+        //    set curl url
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        //    set cur headers
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+
+
+
+        //    data to be posted to the url
+
+        $curl_post_data = array(
+
+            'message' => $msg,
+
+            'to' => $to,
+
+            'subject' =>  $subject,
+
+            'from' => $from
+
+        );
+
+
+
+        //json encode the data
+
+        $data_string = json_encode($curl_post_data);
+
+        //    expect to return value from url
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        //    set method to POST
+
+        curl_setopt($curl, CURLOPT_POST, true);
+
+        //    set the data to post
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+        //    print the response
+
+        print_r(curl_exec($curl));
+
+        // sendMail($to, $msg, $subject, $from = '');
+    }
+
+    // call the funtion to send email
+
+    // sendMail('sb30pu4176216@pu.ac.ke', '<b>hello world</b>', 'mail test', 'Angelo Karugo');
+}
